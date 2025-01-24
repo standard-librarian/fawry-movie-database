5ed9f1421fccfcd78f0ff9ee4869748da313d55a
Medhat Mohammed <medhatmohammed1612@gmail.com>
2025-01-24 17:44:54 +0200

    Use the router

--- movie-management-frontend/src/app/app.component.ts
+++ movie-management-frontend/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'movie-management-frontend';
